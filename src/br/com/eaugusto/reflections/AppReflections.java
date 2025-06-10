1c296da25a7162631165e6283ee999403e128ccc
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-06-10 10:54:45 -0300

    Created AppReflections Main Class
    Utilized Reflections In Main Class To:
    - Get The Class Name Directly;
    - Get The Class Name By Instantiating The Class First;
    - Get The Constructor;
    - Instantiate A Product Through The Constructor;
    - Get The Names And Types Of The Product's Properties;
    - Everything Is Returned And Properly Displayed And Organized In The Console Via "Println()";

---
+++ src/br/com/eaugusto/reflections/AppReflections.java
@@ -0,0 +1,48 @@
+/**
+ * @author Eduardo Augusto (https://github.com/AsrielDreemurrGM/)
+ * @since Jun 10, 2025
+ */
+
+package br.com.eaugusto.reflections;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
+public class AppReflections {
+
+	public static void main(String[] args) {
+
+		System.out.println("Getting The Product Class Directly");
+		Class productClass = Product.class;
+
+		System.out.println(productClass);
+
+		System.out.println("\nGetting The Product Class By Instantiating It First");
+		Product product = new Product();
+		Class productClass2 = product.getClass();
+		System.out.println(productClass2);
+
+		try {
+			System.out.println("\nGetting The Product Class Constructor");
+			Constructor cons = productClass.getConstructor();
+			System.out.println("Constructor: " + cons);
+
+			System.out.println("\nGetting The Product Created Through The Constructor");
+			Product prod1 = (Product) cons.newInstance();
+			System.out.println("Product: " + prod1);
+
+			System.out.println("\nGetting All The Product's Properties And Their Respective Types");
+			Field[] fields = prod1.getClass().getDeclaredFields();
+			for (Field eachField : fields) {
+				String name = eachField.getName();
+				Class<?> type = eachField.getType();
+				System.out.println("Field Name: " + name + " | Type: " + type);
+			}
+
+		} catch (NoSuchMethodException | InstantiationException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+}
