4a7d292ffa3cf1909137a54e6aa203954ec5bc7d
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-06-11 10:45:26 -0300

    Added Javadocs To KeyType Annotation

--- src/br/com/eaugusto/reflections/annotations/annotation/KeyType.java
+++ src/br/com/eaugusto/reflections/annotations/annotation/KeyType.java
@@ -7,6 +7,30 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * Indicates which method should be used to retrieve the unique identifier of a
+ * field annotated with this annotation.
+ * 
+ * <p>
+ * This annotation must be applied to a field in a class that implements
+ * {@link br.com.eaugusto.reflections.annotations.domain.Persistable}.
+ * </p>
+ * 
+ * <p>
+ * The value of this annotation should match the name of a no-argument public
+ * method (usually a getter) that returns the unique identifier for the object
+ * as a {@link String}.
+ * </p>
+ * 
+ * <p>
+ * Example:
+ * </p>
+ *
+ * <pre>
+ * @code
+ * KeyType("getCpf")
+ * private String cpf;
+ * </pre>
+ * 
  * @author Eduardo Augusto (https://github.com/AsrielDreemurrGM/)
  * @since Jun 11, 2025
  */
